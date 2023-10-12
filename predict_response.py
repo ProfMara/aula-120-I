dc45ef4248ee98043cd90016ba78dc6178f7fa4b
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-10-12 12:44:42 -0300

    Add files via upload

---
+++ predict_response.py
@@ -0,0 +1,11 @@
+# Bibliotecas de pré-processamento de dados de texto
+import nltk
+
+import json
+import pickle
+import numpy as np
+import random
+
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+
+# Biblioteca load_model
---
+++ train_bot.py
@@ -0,0 +1,7 @@
+# Bibliotecas de treinamento do modelo
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import Adam
+
+
+
